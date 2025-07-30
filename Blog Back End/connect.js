60398f6d699616ae9cc57e06555d4a8f236a92fd
Kush <kushkodiya42@gmail.com>
2025-07-30 19:04:03 +0530

    Edited connect.js to fix connection issues

--- Blog Back End/connect.js
+++ Blog Back End/connect.js
@@ -2,9 +2,12 @@ const mongoose = require('mongoose');
 
 async function connectToMongoDb(url) {
     return mongoose
-        .connect(url)
-        .then(() => {})
-        .catch((err) => {});
+        .connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        .then(() => {console.log('Connected to MongoDB');})
+        .catch((err) => {console.error('MongoDB connection error:', err);});
 }
 
 module.exports = {
