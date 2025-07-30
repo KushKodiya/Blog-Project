eefaa4bdb9e75864238c8d48d9ab7860dce5ff72
Kush <kushkodiya42@gmail.com>
2025-07-30 19:57:52 +0530

    Updated vite.config file

--- Blog Back End/connect.js
+++ Blog Back End/connect.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectToMongoDb(url) {
     return mongoose
-        .connect(url, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
+        .connect(url)
         .then(() => {console.log('Connected to MongoDB');})
         .catch((err) => {console.error('MongoDB connection error:', err);});
 }
